bcfce684212af9d256968f730e0b38d7b99464f8
20108601_Lộc Trường Luân <98070613+luan2410@users.noreply.github.com>
2025-09-23 18:09:50 +0700

    sua loi api gate way

--- backend/api-gateway-bff/pages/_app.tsx
+++ backend/api-gateway-bff/pages/_app.tsx
@@ -40,7 +40,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
       
       {isLoading ? (
